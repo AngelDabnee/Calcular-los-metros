da15a50e663ef1afb2739c0b619788b5b017f130
Angel Dabnee Gonzalez Rodriguez <a22311046@uthermosillo.edu.mx>
2023-01-09 14:59:29 -0700

    primer commit

---
+++ Ejercicio 27. Metros que se necesitan/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Permite que usuario capture la cantidad de cuantos metros de tubo se necesitan instalar en una construcción. Imprime cuantas pulgadas son su equivalente.");
+//Datos de entrada
+double metros = 0;
+double pulgadas = 0;
+const double eqPulgada = 39.3701;//Equivalente de una pulgda a metro
+//Proceso
+Console.WriteLine("Captura los metros de tubo que necesitas ");
+metros = double.Parse(Console.ReadLine());
+pulgadas = metros * eqPulgada;
+//Datos de salida
+Console.WriteLine($"La cantidad de pulgadas que necesitas es {pulgadas} in");
